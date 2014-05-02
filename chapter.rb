c5a3430661fa21a9811ee70ccbcfbde455c95d20
Nicole Lewis <lewis.nicole@gmail.com>
2014-05-01 20:35:56 -0700

    Inital Commit

---
+++ chapter.rb
@@ -0,0 +1,17 @@
+@chapters = {
+  1 => "introduction.xhtml",
+  2..5 => "chapter1.xhtml",
+  6..10 => "chapter2.xhtml",
+  11..18 => "chapter3.xhtml",
+  19..30 => "chapter4.xhtml" }
+
+def find_chapter(number)
+  @chapters.each do |page_range, chapter_name|
+    if page_range === number
+      puts chapter_name
+    end
+  end
+end
+
+find_chapter(1)
+find_chapter(15)
